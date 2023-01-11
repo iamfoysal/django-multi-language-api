5b09f776b0dc334131119382e942d621294cf153
pddjr <46605025+codeditors@users.noreply.github.com>
2023-01-12 03:06:35 +0600

    Update views.py

--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,3 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
 from rest_framework import viewsets
 from .serializers import *
 from .models import *
